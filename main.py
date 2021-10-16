1439c22237fefe3d372cf2a9151a248e50c932be
Proctor007 <77384665+Proctor007@users.noreply.github.com>
2021-10-17 00:21:17 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import zipfile
+filelist = []
+progress=0
+path = input()
+for root, dirs, files in os.walk(path):
+    for file in files:
+        filelist.append(os.path.join(root,file))
+for files in filelist:
+    newfiledir=files.replace(path, path + "zipped")
+    while newfiledir[len(newfiledir) - 1] != "\\":
+        newfiledir = newfiledir[:-1]
+    newfiledir = newfiledir[:-1]
+    os.makedirs(newfiledir,exist_ok=True)
+    archive = zipfile.ZipFile(files.replace(path, path + "zipped"), mode='w')
+    print(files)
+    archmame=files.replace(path, "")
+    while "\\" in archmame:
+        archmame=archmame[1:]
+    archive.write(files, arcname=archmame,compress_type=zipfile.ZIP_DEFLATED )
+    archive.close()
+    progress=progress+1
+    print(f"progress {progress}/{len(filelist)}")
